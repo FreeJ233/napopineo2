98d73d8cf42aea045c0900705a53ebb55c995e14
FreeJ233 <50964071+FreeJ233@users.noreply.github.com>
2022-11-10 10:40:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
-echo 'src-git istore https://github.com/linkease/istore.git;main' >> feeds.conf.default
+#echo 'src-git istore https://github.com/linkease/istore.git;main' >> feeds.conf.default
